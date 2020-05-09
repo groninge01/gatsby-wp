543ba2932d0b0436a87c5a21ddb32689e8b499e4
groninge01 <groninge@hotmail.com>
2020-05-09 16:44:00 +0200

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-top-layout',
+    {
+      resolve: 'gatsby-plugin-material-ui',
+      // If you want to use styled components you should change the injection order.
+      options: {
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
+      },
+    },
+    // If you want to use styled components you should add the plugin here.
+    // 'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'WPGraphQL',
+        fieldName: 'wpgraphql',
+        url: 'https://test.vitasupport.nl/graphql',
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'My page',
+  },
+};
