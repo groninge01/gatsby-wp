738081cdba92bd953efc5f9b3925b20f742cc208
groninge01 <groninge@hotmail.com>
2020-05-10 15:21:46 +0200

    add wp settings and menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import Link from './link';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import '@wordpress/block-library/build-style/style.css';
 
 const Layout = ({ children }) => {
+  const menu = useStaticQuery(graphql`
+    query {
+      wpgraphql {
+        generalSettings {
+          title
+          url
+        }
+        menu(id: "TWVudToy") {
+          menuItems {
+            nodes {
+              id
+              label
+              url
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const { title, url } = menu.wpgraphql.generalSettings;
+  // loop through the menu items and make the links relative
+  const items = menu.wpgraphql.menu.menuItems.nodes.map((item) => ({
+    ...item,
+    url: item.url.replace(url, ''),
+  }));
+
   return (
     <>
       <header>
         <Link to="/" className="home">
-          Migrate WordPress to the Jamstack
+          {title}
         </Link>
+
+        {items.map((item) => (
+          <Link key={item.url} to={item.url}>
+            {item.label}
+          </Link>
+        ))}
       </header>
       <main>{children}</main>
     </>
