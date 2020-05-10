1802a7136b61bb23bc6a2d903556652952b3f0dd
groninge01 <groninge@hotmail.com>
2020-05-10 11:06:30 +0200

    use kebab case

--- plugins/gatsby-plugin-top-layout/gatsby-ssr.js
+++ plugins/gatsby-plugin-top-layout/gatsby-ssr.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export, react/prop-types */
 import React from 'react';
-import TopLayout from './TopLayout';
+import TopLayout from './top-layout';
 
 export const wrapRootElement = ({ element }) => {
   return <TopLayout>{element}</TopLayout>;
